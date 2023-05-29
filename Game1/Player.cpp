cc887e6a43a7112dd56d4a03d04eeedc9038a8b2
pkh555 <134608073+changeNameTo@users.noreply.github.com>
2023-05-29 15:44:00 +0900

    player Update
    이동함수 및 애니메이션 관련 함수 추가
    (실행 안해봄 확인 필요)

--- Game1/Player.cpp
+++ Game1/Player.cpp
@@ -7,7 +7,6 @@ Player* Player::Create(string name)
 	player->LoadFile("TossMan.xml");
 
 
-
 	return player;
 }
 
@@ -18,6 +17,47 @@ void Player::Release()
 		SafeRelease(it->second);
 	}
 	delete this;
+}
+
+void Player::MovePlayer(float scalar, float seta)
+{
+	// ZY 평면에서의 벡터값에 중력 뺀 방향으로 이동시키는 함수인데 의미가 있나...?
+	Find("root")->MoveWorldPos(((Vector3(0.0f, sin(seta), cos(seta)) * scalar) - (UP * gravity)) * DELTA);
+}
+
+void Player::PlayerAnimationPosSetting(Actor* Animation, int size)
+{
+	// 애니메이션 부르기 전에 기존에 불러왔던 xml들 초기 위치를 플레이어 위치로 이동시킴
+	for (int i = 0; i < size; i++)
+	{
+		Animation[size].SetWorldPos(Find(root->name)->GetWorldPos());
+	}
+}
+
+// 수업시간에 쓴 애니메이션 함수 복붙
+void Player::Animation(GameObject* root)
+{
+	GameObject* _src = Ani[src].Find(root->name);
+	GameObject* _dest = Ani[dest].Find(root->name);
+	Vector3 CurrentPos = _src->GetWorldPos();
+	root->SetLocalPos(Util::Lerp(_src->GetLocalPos(),
+		_dest->GetLocalPos(), t));
+	//root->rotation =
+	//Util::QuaternionToYawPtichRoll(
+	//    Util::Lerp(
+	//        Quaternion::CreateFromYawPitchRoll(_src->rotation.y, _src->rotation.x, _src->rotation.z),
+	//        Quaternion::CreateFromYawPitchRoll(_dest->rotation.y, _dest->rotation.x, _dest->rotation.z)
+	//        , t));
+
+	root->rotation = Util::Lerp(_src->rotation, _dest->rotation, t);
+
+	for (auto it = root->children.begin();
+		it != root->children.end(); it++)
+	{
+		Animation(it->second);
+	}
+
+	_dest->SetWorldPos(CurrentPos);
 }
 
 Vector3 Player::GetPlayerWorldPos()
@@ -68,6 +108,7 @@ float Player::GetPlayerRotationZ()
 	GameObject* Rot = Find("TossMan");
 	return Rot->rotation.z;
 }
+
 
 Player::Player()
 {
--- Game1/Player.h
+++ Game1/Player.h
@@ -6,16 +6,26 @@ public:
     Player();
     virtual ~Player();
 
-    
+    // player's move velocity scalar
+    float   scalar;
+    // player's move velocity rotation seta
+    // 근데 이거 seta 값을 ToRadian 해야하나 말아야하나 모름
+    float   seta;
+    float   gravity;
+
+    class Actor* Ani;
+    int src, dest;
+    float t = 0.0f;
+
 public:
     void	Update() override;
     void	Release() override;
     
-    //플레이어 root 위치
+    //player root worldPos
     Vector3 GetPlayerWorldPos();
     void    SetPlayerWorldPos(Vector3 pos);
 
-    //플레이어 root 회전값
+    //player root rotation
     void    SetPlayerRotationX(float rot);
     void    SetPlayerRotationY(float rot);
     void    SetPlayerRotationZ(float rot);
@@ -23,5 +33,22 @@ public:
     float   GetPlayerRotationX();
     float   GetPlayerRotationY();
     float   GetPlayerRotationZ();
+
+    // player's scalar and shooting rotation seta setting
+    void    SetScalar(float scalar) { this->scalar = scalar; }
+    void    SetSeta(float seta) { this->seta = seta; }
+
+    // player's gravity get, set
+    void    SetGravity(float gravity) { this->gravity = gravity; }
+    float   GetGravity() { return gravity; }
+
+    // player's Move WorldPos
+    void    MovePlayer(float scalar, float seta);
+
+    // if player's animation change, animation's pos setting
+    void    PlayerAnimationPosSetting(Actor* Animation, int size);
+
+    // animation function
+    void Animation(GameObject* root);
 };
 
