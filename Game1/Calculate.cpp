259128b486c5ffc4c150762d953878672333658f
Hangyubin0417 <bin66899@gmail.com>
2023-05-31 19:07:00 +0900

    Update Calculate.cpp

--- Game1/Calculate.cpp
+++ Game1/Calculate.cpp
@@ -3,12 +3,15 @@
 #include "Cannon.h"
 #include "Player.h"
 #include "Airplane.h"
+#include "MainUI.h"
 #include "ObjectPool.h"
 
 void Calculate::Update()
 {
+
 }
 
 void Calculate::Release()
 {
+
 }
